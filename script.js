01de515f7737eb20d3d9f8b67b51063de0255a23
AKSHAT249 <92660619+AKSHAT249@users.noreply.github.com>
2024-11-05 22:43:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-//your JS code here. If required.
+let para  =document.getElementById("para");
+function date(){
+
+    let d = new Date();
+    let typee="";
+    let year = d.getFullYear();
+    let month = d.getMonth();
+    let date = d.getDate();
+    let hours = d.getHours();
+    let minutes = d.getMinutes();
+    let seconds = d.getSeconds();
+    if(hours>=0 && hours<12){
+         typee="AM"
+    }else{
+         typee="PM"
+    }
+    let ans = `${month}/${date}/${year}, ${hours}:${minutes}:${seconds} ${typee}`;
+    para.innerHTML = ""
+	para.innerHTML = ans;
+
+}
+
+
+setInterval(date,1000);
